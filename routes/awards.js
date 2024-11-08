999287fde93eb652fd56eca7a4637d053ca5162a
Khushi Jirge <Khushi.Jirge@MRETillsonburg.com>
2024-11-08 15:59:02 -0500

    more work

--- routes/awards.js
+++ routes/awards.js
@@ -3,18 +3,15 @@ const path = require('path');
 const fs = require('fs');
 const router = express.Router();
 
-// Serve the scraped awards data to the view
 router.get('/awards', (req, res) => {
-    // Read the JSON file containing the scraped data
     fs.readFile(path.join(__dirname, 'Book\StoryBook\public\awards_data.json'), 'utf8', (err, data) => {
         if (err) {
             console.error('Error reading the JSON file:', err);
             return res.status(500).send('Internal Server Error');
         }
 
-        const awards = JSON.parse(data);  // Parse the JSON data
+        const awards = JSON.parse(data);
 
-        // Pass the awards data to the Handlebars view
         res.render('awards', { awards });
     });
 });
